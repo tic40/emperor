771bab3a416700e57a02d20498cbc5a74d7545b1
tic40 <ccpzjoh@gmail.com>
2018-05-04 22:19:39 +0900

    init: create rails API app and react/redux template

---
+++ app/javascript/packs/containers/IndexContainer.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+
+class IndexContainer extends Component {
+  render () {
+    return (
+      <div>
+        <div>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+        </div>
+        <div>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+        </div>
+        <div>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+        </div>
+        <div>
+          <button>0</button>
+          <button>+</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default IndexContainer
