b03b0a48edfe4d6ceb8cb3381748c743edf4c69f
tic40 <ccpzjoh@gmail.com>
2018-05-05 01:20:26 +0900

    deploy test

--- app/javascript/packs/containers/IndexContainer.js
+++ app/javascript/packs/containers/IndexContainer.js
@@ -4,25 +4,7 @@ class IndexContainer extends Component {
   render () {
     return (
       <div>
-        <div>
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-        </div>
-        <div>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-        </div>
-        <div>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-        </div>
-        <div>
-          <button>0</button>
-          <button>+</button>
-        </div>
+        hello world.
       </div>
     )
   }
