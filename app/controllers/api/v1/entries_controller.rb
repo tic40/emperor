3c58509129ce251eab0ede195853ca70e60cb693
tic40 <ccpzjoh@gmail.com>
2018-05-10 01:29:03 +0900

    cache test

--- app/controllers/api/v1/entries_controller.rb
+++ app/controllers/api/v1/entries_controller.rb
@@ -1,12 +1,16 @@
 class Api::V1::EntriesController < ApplicationController
 
   def index
-    entries = Entry.all
+    entries = Rails.cache.fetch("api/v1/entries", expires_in: 5.minutes) do
+      Entry.all
+    end
     render json: entries, include: [:comments, :feed]
   end
 
   def show
-    entries = Entry.where(feed_id: params[:id])
+    entries = Rails.cache.fetch("api/v1/entryes/#{params[:id]}", expires_in: 5.minutes) do
+      Entry.where(feed_id: params[:id])
+    end
     render json: entries, include: [:comments, :feed]
   end
 end
